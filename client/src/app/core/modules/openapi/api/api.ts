c8fba432f914aa8e502fd6828a1ebf4d1126c15e
Turker Koc <turker.koc@tum.de>
2024-11-11 17:29:35 +0100

    init client openapi

---
+++ client/src/app/core/modules/openapi/api/api.ts
@@ -0,0 +1,4 @@
+export * from './status-controller.service';
+import { StatusControllerService } from './status-controller.service';
+export * from './status-controller.serviceInterface';
+export const APIS = [StatusControllerService];
