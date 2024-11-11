ee0e7cfe75b68b7c4c748261fa17cb853b2a3df3
Stefan Németh <me@stefannemeth.com>
2024-11-11 12:45:10 +0100

    chore(server): Initial Spring boot server setup (#10)

---
+++ server/application-server/src/main/java/de/tum/cit/aet/helios/StatusController.java
@@ -0,0 +1,18 @@
+package de.tum.cit.aet.helios;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.http.HttpStatus;
+
+@RestController
+@RequestMapping("/status")
+public class StatusController {
+    @GetMapping("/health")
+    @ResponseStatus(HttpStatus.OK)
+    public String healthCheck() {
+        return "ok";
+    }
+}
