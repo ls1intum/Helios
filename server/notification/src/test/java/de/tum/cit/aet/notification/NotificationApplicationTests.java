ade16ae8df630be264a7777c6e17fdc17366cb0e
Turker Koc <turker.koc@tum.de>
2024-11-15 00:53:23 +0100

    init spring boot

---
+++ server/notification/src/test/java/de/tum/cit/aet/notification/NotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package de.tum.cit.aet.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationApplicationTests {
+
+	// @Test
+	// void contextLoads() {
+	// }
+
+}
