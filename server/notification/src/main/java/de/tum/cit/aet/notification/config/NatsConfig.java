d045412bdf5506ad053a0af7252085af912e7908
Turker Koc <39654393+TurkerKoc@users.noreply.github.com>
2024-11-17 18:32:16 +0100

    chore: Setup notification service and change Java to LTS 21 (#29)

---
+++ server/notification/src/main/java/de/tum/cit/aet/notification/config/NatsConfig.java
@@ -0,0 +1,28 @@
+package de.tum.cit.aet.notification.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+import io.nats.client.Connection;
+import io.nats.client.Nats;
+import io.nats.client.Options;
+
+@Configuration
+public class NatsConfig {
+
+    @Value("${nats.server}")
+    private String natsServer;
+
+    @Value("${nats.auth.token}")
+    private String natsAuthToken;
+
+    @Bean
+    public Connection natsConnection() throws Exception {
+
+        Options options = Options.builder().server(natsServer).token(natsAuthToken).build();
+        return Nats.connect(options);
+    }
+}
