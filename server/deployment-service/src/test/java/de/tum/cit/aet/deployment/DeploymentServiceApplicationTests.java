4b4037330349002493d095dcdfa520556d15e5c9
bakirova <galiiabanu.bakirova@tum.de>
2024-11-22 03:10:13 +0100

    feat: initialize deployment service

---
+++ server/deployment-service/src/test/java/de/tum/cit/aet/deployment/DeploymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.tum.cit.aet.deployment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeploymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
