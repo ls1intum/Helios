79dfebfe8af0341fd387f85738fa0a3879fcbcbd
bakirova <galiiabanu.bakirova@tum.de>
2024-11-25 00:51:05 +0100

    Revert "Merge environment controlller"
    This reverts commit 168fd974310143fb65bc5cfef047fc010611fa53.

--- server/application-server/src/main/java/de/tum/cit/aet/helios/environments/installed_applications/InstalledApplicationRepository.java
+++
@@ -1,7 +0,0 @@
-package de.tum.cit.aet.helios.environments.installed_applications;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InstalledApplicationRepository extends JpaRepository<InstalledApplication, Long> {
-
-}
