7369ec97256e945b9afe123388ef20f7606e54b9
Stefan Nemeth <stefan@bizfactory.tech>
2024-11-26 13:02:15 +0100

    Do not sync pull request twice

--- server/application-server/src/main/java/de/tum/cit/aet/helios/workflow/github/GitHubWorkflowRunMessageHandler.java
+++ server/application-server/src/main/java/de/tum/cit/aet/helios/workflow/github/GitHubWorkflowRunMessageHandler.java
@@ -6,25 +6,21 @@ import org.springframework.stereotype.Component;
 
 import de.tum.cit.aet.helios.github.GitHubMessageHandler;
 import de.tum.cit.aet.helios.gitrepo.github.GitHubRepositorySyncService;
-import de.tum.cit.aet.helios.pullrequest.github.GitHubPullRequestSyncService;
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 @Component
 public class GitHubWorkflowRunMessageHandler extends GitHubMessageHandler<GHEventPayload.WorkflowRun> {
     private final GitHubRepositorySyncService repositorySyncService;
-    private final GitHubPullRequestSyncService pullRequestSyncService;
     private final GitHubWorkflowSyncService workflowSyncService;
 
     private GitHubWorkflowRunMessageHandler(
             GitHubWorkflowSyncService workflowSyncService,
-            GitHubRepositorySyncService repositorySyncService,
-            GitHubPullRequestSyncService pullRequestSyncService) {
+            GitHubRepositorySyncService repositorySyncService) {
         super(GHEventPayload.WorkflowRun.class);
 
         this.workflowSyncService = workflowSyncService;
         this.repositorySyncService = repositorySyncService;
-        this.pullRequestSyncService = pullRequestSyncService;
     }
 
     @Override
@@ -39,15 +35,6 @@ public class GitHubWorkflowRunMessageHandler extends GitHubMessageHandler<GHEven
                 action);
 
         repositorySyncService.processRepository(eventPayload.getRepository());
-
-        try {
-            run.getPullRequests().forEach(pullRequest -> {
-                pullRequestSyncService.processPullRequest(pullRequest);
-            });
-        } catch (Exception e) {
-            log.error("Failed to process pull requests for workflow run {}: {}", run.getUrl(), e.getMessage());
-        }
-
         workflowSyncService.processRun(run);
     }
 
