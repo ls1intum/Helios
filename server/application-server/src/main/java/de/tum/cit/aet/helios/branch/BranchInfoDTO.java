e12c6ee387176248c0a7b66f70dd0e61829578d4
Turker Koc <turker.koc@tum.de>
2024-12-03 00:08:11 +0100

    server branches commit_sha added with openapi

--- server/application-server/src/main/java/de/tum/cit/aet/helios/branch/BranchInfoDTO.java
+++ server/application-server/src/main/java/de/tum/cit/aet/helios/branch/BranchInfoDTO.java
@@ -3,26 +3,22 @@ package de.tum.cit.aet.helios.branch;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 import de.tum.cit.aet.helios.gitrepo.RepositoryInfoDTO;
-import de.tum.cit.aet.helios.issue.Issue.State;
-import de.tum.cit.aet.helios.user.UserInfoDTO;
 
 import org.springframework.lang.NonNull;
-
-import java.time.OffsetDateTime;
-import java.util.Comparator;
-import java.util.List;
 
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 public record BranchInfoDTO(
         @NonNull String name,
+        @NonNull String commit_sha,
         RepositoryInfoDTO repository
         ) {
 
     public static BranchInfoDTO fromBranch(Branch branch) {
         return new BranchInfoDTO(
                 branch.getName(),
+                branch.getCommit_sha(),
                 RepositoryInfoDTO.fromRepository(branch.getRepository())
-                );
+        );
     }
 
 }
