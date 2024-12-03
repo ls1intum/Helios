1a4c359df1130100521772d86cdb14fda08f8331
Turker Koc <turker.koc@tum.de>
2024-12-03 12:23:33 +0100

    branch bug fix

--- client/src/app/components/branches-table/branches-table.component.ts
+++ client/src/app/components/branches-table/branches-table.component.ts
@@ -1,38 +1,64 @@
-import { Component, inject, signal } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, inject, Injectable, signal } from '@angular/core';
+
 import { TableModule } from 'primeng/table';
 import { AvatarModule } from 'primeng/avatar';
 import { TagModule } from 'primeng/tag';
 import { injectQuery } from '@tanstack/angular-query-experimental';
-import { tap } from 'rxjs';
+import { catchError, tap } from 'rxjs';
 import { IconsModule } from 'icons.module';
 import { BranchControllerService, BranchInfoDTO } from '@app/core/modules/openapi';
+import { SkeletonModule } from 'primeng/skeleton';
 
 
 @Component({
   selector: 'app-branches-table',
-  imports: [CommonModule, TableModule, AvatarModule, TagModule, IconsModule],
+  imports: [TableModule, AvatarModule, TagModule, IconsModule, SkeletonModule],
   templateUrl: './branches-table.component.html',
 })
 export class BranchTableComponent {
   branchService = inject(BranchControllerService);
+  branchStore = inject(BranchStoreService);
 
+  isError = signal(false);
+  isEmpty = signal(false);
+  isLoading = signal(false);
 
-  branches = signal<BranchInfoDTO[]>([]);
-  isLoading = signal(true);
 
   query = injectQuery(() => ({
     queryKey: ['branches'],
-    queryFn: () => this.branchService.getAllBranches()
+    queryFn: () => {
+      this.isLoading.set(true);
+      return this.branchService.getAllBranches()
         .pipe(
           tap(data => {
-            this.branches.set(data);
+            this.branchStore.setBranches(data);
+            this.isEmpty.set(data.length === 0);
             this.isLoading.set(false);
-          })
-        ).subscribe(),
+          }),
+          catchError(() => {
+            this.isError.set(true);
+            this.isLoading.set(false);
+            return [];
+          }
+          )
+        ).subscribe()
+    },
   }));
 
   // openBranch(url: string): void {
   //   window.open(url, '_blank');
   // }
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BranchStoreService {
+  private branchesState = signal<BranchInfoDTO[]>([]);
+  get branches() {
+    return this.branchesState.asReadonly();
+  }
+  setBranches(branches: BranchInfoDTO[]) {
+    this.branchesState.set(branches);
+  }
 }
