d46fde5b815a67c8a38ad9ba6ba0b3583fc18c20
Galiiabanu Bakirova <56745022+gbanu@users.noreply.github.com>
2024-12-03 11:41:13 +0100

    feat: Frontend improvements (#71)

--- client/src/app/components/pull-request-table/pull-request-table.component.ts
+++ client/src/app/components/pull-request-table/pull-request-table.component.ts
@@ -1,17 +1,18 @@
-import { Component, inject, signal } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, inject, Injectable, signal } from '@angular/core';
+
 import { TableModule } from 'primeng/table';
 import { AvatarModule } from 'primeng/avatar';
 import { TagModule } from 'primeng/tag';
 import { injectQuery } from '@tanstack/angular-query-experimental';
-import { tap } from 'rxjs';
+import { catchError, tap } from 'rxjs';
 import { IconsModule } from 'icons.module';
 import { PullRequestControllerService, PullRequestInfoDTO } from '@app/core/modules/openapi';
+import { SkeletonModule } from 'primeng/skeleton';
 
 
 @Component({
   selector: 'app-pull-request-table',
-  imports: [CommonModule, TableModule, AvatarModule, TagModule, IconsModule],
+  imports: [TableModule, AvatarModule, TagModule, IconsModule, SkeletonModule],
   templateUrl: './pull-request-table.component.html',
   styles: [`
     :host ::ng-deep {
@@ -27,21 +28,33 @@ import { PullRequestControllerService, PullRequestInfoDTO } from '@app/core/modu
 })
 export class PullRequestTableComponent {
   pullRequestService = inject(PullRequestControllerService);
+  pullRequestStore = inject(PullRequestStoreService);
 
-
-  pullRequests = signal<PullRequestInfoDTO[]>([]);
-  isLoading = signal(true);
+  isError = signal(false);
+  isEmpty = signal(false);
+  isLoading = signal(false)
 
   query = injectQuery(() => ({
     queryKey: ['pullRequests'],
-    queryFn: () => this.pullRequestService.getAllPullRequests()
+    queryFn: () => {
+      this.isLoading.set(true);
+      return this.pullRequestService.getAllPullRequests()
         .pipe(
           tap(data => {
-            this.pullRequests.set(data);
+            this.pullRequestStore.setPullRequests(data);
+            this.isEmpty.set(data.length === 0);
             this.isLoading.set(false);
-          })
-        ).subscribe(),
+          }),
+          catchError(() => {
+            this.isError.set(true);
+            this.isLoading.set(false);
+            return [];
+          }
+          )
+        ).subscribe()
+    },
   }));
+
 
   getStatus(pr: PullRequestInfoDTO): string {
     if (pr.isMerged) return 'Merged';
@@ -63,5 +76,20 @@ export class PullRequestTableComponent {
 
   openPR(url: string): void {
     window.open(url, '_blank');
+  }
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PullRequestStoreService {
+  private pullRequestsState = signal<PullRequestInfoDTO[]>([]);
+
+  get pullRequests() {
+    return this.pullRequestsState.asReadonly();
+  }
+
+  setPullRequests(pullRequests: PullRequestInfoDTO[]) {
+    this.pullRequestsState.set(pullRequests);
   }
 }
