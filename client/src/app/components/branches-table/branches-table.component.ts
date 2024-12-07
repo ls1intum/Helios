083202881474813d9268e35a9b5c4bae552cd0b9
bakirova <galiiabanu.bakirova@tum.de>
2024-12-07 18:29:53 +0100

    feat: show special branches first

--- client/src/app/components/branches-table/branches-table.component.ts
+++ client/src/app/components/branches-table/branches-table.component.ts
@@ -1,6 +1,6 @@
-import { Component, inject, Injectable, signal } from '@angular/core';
+import { Component, inject, Injectable, signal, ViewChild } from '@angular/core';
 
-import { TableModule } from 'primeng/table';
+import { Table, TableModule } from 'primeng/table';
 import { AvatarModule } from 'primeng/avatar';
 import { TagModule } from 'primeng/tag';
 import { injectQuery } from '@tanstack/angular-query-experimental';
@@ -8,20 +8,40 @@ import { catchError, tap } from 'rxjs';
 import { IconsModule } from 'icons.module';
 import { BranchControllerService, BranchInfoDTO } from '@app/core/modules/openapi';
 import { SkeletonModule } from 'primeng/skeleton';
+import { InputIconModule } from 'primeng/inputicon';
+import { InputTextModule } from 'primeng/inputtext';
+import { IconFieldModule } from 'primeng/iconfield';
 
 
 @Component({
   selector: 'app-branches-table',
-  imports: [TableModule, AvatarModule, TagModule, IconsModule, SkeletonModule],
+  imports: [TableModule, AvatarModule, TagModule, IconsModule, SkeletonModule, InputTextModule,
+    IconFieldModule,
+    InputIconModule],
   templateUrl: './branches-table.component.html',
 })
 export class BranchTableComponent {
+
   branchService = inject(BranchControllerService);
   branchStore = inject(BranchStoreService);
 
   isError = signal(false);
   isEmpty = signal(false);
   isLoading = signal(false);
+
+  specialBranches = ['master', 'main', 'dev', 'staging', 'development', 'prod', 'production'];
+
+  getSpecialBranches() {
+    return this.branchStore.branches().filter(branch =>
+      this.specialBranches.includes(branch.name.toLowerCase())
+    );
+  }
+
+  getFeatureBranches() {
+    return this.branchStore.branches().filter(branch =>
+      !this.specialBranches.includes(branch.name.toLowerCase())
+    );
+  }
 
 
   query = injectQuery(() => ({
@@ -54,7 +74,7 @@ export class BranchTableComponent {
   providedIn: 'root'
 })
 export class BranchStoreService {
-  private branchesState = signal<BranchInforWithLink[]>([]);
+  private branchesState = signal<BranchInfoWithLink[]>([]);
   get branches() {
     return this.branchesState.asReadonly();
   }
@@ -64,8 +84,8 @@ export class BranchStoreService {
       link: `https://github.com/${branch!.repository!.nameWithOwner}/tree/${branch.name}`,
       lastCommitLink: `https://github.com/${branch!.repository!.nameWithOwner}/commit/${branch.commit_sha}`
     }));
-    this.branchesState.set(branches as BranchInforWithLink[]);
+    this.branchesState.set(branches as BranchInfoWithLink[]);
   }
 }
 
-export type BranchInforWithLink = BranchInfoDTO & { link: string, lastCommitLink: string };
+export type BranchInfoWithLink = BranchInfoDTO & { link: string, lastCommitLink: string };
