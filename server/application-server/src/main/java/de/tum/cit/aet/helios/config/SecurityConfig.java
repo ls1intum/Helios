b6aaee428e1676924170a405bd9a7b47a4f35df1
Turker Koc <turker.koc@tum.de>
2024-12-09 15:16:52 +0300

    keycloak server changes

--- server/application-server/src/main/java/de/tum/cit/aet/helios/config/SecurityConfig.java
+++ server/application-server/src/main/java/de/tum/cit/aet/helios/config/SecurityConfig.java
@@ -9,8 +9,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
