ffbe4cebcaefeba77f0ed6031b59f417a1ec93c9
Galiiabanu Bakirova <56745022+gbanu@users.noreply.github.com>
2024-12-10 11:58:10 +0100

    feat: Implement UI feedback (#79)

--- client/src/app/pages/pull-request-pipeline/pull-request-pipeline.component.ts
+++ client/src/app/pages/pull-request-pipeline/pull-request-pipeline.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, input, Signal } from '@angular/core';
+import { Component, computed, inject, input, signal, Signal } from '@angular/core';
 import { Pipeline, PipelineService } from '@app/core/services/pipeline';
 import { PipelineComponent } from '@app/components/pipeline/pipeline.component';
 import { FetchEnvironmentService } from '@app/core/services/fetch/environment';
@@ -11,15 +11,25 @@ import { IconsModule } from 'icons.module';
 import { EnvironmentCommitInfoComponent } from '@app/components/environment-commit-info/environment-commit-info.component';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
+import { PullRequestStoreService } from '@app/core/services/pull-requests';
+import { MarkdownPipe } from '@app/core/modules/markdown/markdown.pipe';
 
 @Component({
   selector: 'app-pull-request-pipeline',
-  imports: [InputTextModule, AccordionModule, CommonModule, LockTagComponent, RouterLink, TagModule, IconsModule, EnvironmentCommitInfoComponent, ButtonModule, PipelineComponent],
+  imports: [InputTextModule, AccordionModule, CommonModule, LockTagComponent, RouterLink, TagModule, IconsModule, EnvironmentCommitInfoComponent, ButtonModule, PipelineComponent, MarkdownPipe],
   providers: [FetchEnvironmentService],
   templateUrl: './pull-request-pipeline.component.html',
+  styleUrl: './pull-request-pipeline.component.css'
 })
 export class PullRequestPipelineComponent {
   pullRequestId: Signal<number> = input.required();
   fetchEnvironments = inject(FetchEnvironmentService);
   environments = this.fetchEnvironments.getEnvironments().data;
+  pullRequestStore = inject(PullRequestStoreService);
+  currentPullRequest = computed(() => {
+    const prs = this.pullRequestStore.pullRequests();
+    const id = this.pullRequestId();
+    const found = prs.find(pr => pr.id.toString() === id.toString());
+    return found;
+  });
 }
