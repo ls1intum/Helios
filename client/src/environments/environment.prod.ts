df2d797cfe0967a99ed217dbb1031b29e84b65b8
Turker Koc <turker.koc@tum.de>
2024-12-10 13:58:03 +0300

    keycloak prod setup

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,11 @@
 export const environment = {
   production: true,
   serverUrl: 'https://helios.artemis.cit.tum.de',
+  keycloak: {
+    url: 'http://helios.artemis.cit.tum.de',
+    realm: 'helios',
+    clientId: 'helios-app',
+    skipLoginPage: true
+  },
+  clientUrl: 'http://helios.artemis.cit.tum.de'
 };
