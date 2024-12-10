a1958e8f7359822d5c1b657ff857fc67e2f52530
bakirova <galiiabanu.bakirova@tum.de>
2024-12-10 13:42:02 +0100

    fix: link to deployment history

--- client/src/app/pages/pull-request-pipeline/pull-request-pipeline.component.ts
+++ client/src/app/pages/pull-request-pipeline/pull-request-pipeline.component.ts
@@ -1,23 +1,22 @@
 import { Component, computed, inject, input, Signal } from '@angular/core';
 import { PullRequestStoreService } from '@app/core/services/pull-requests';
 import { MarkdownPipe } from '@app/core/modules/markdown/markdown.pipe';
-import {CommonModule} from '@angular/common';
-import {PipelineComponent} from '@app/components/pipeline/pipeline.component';
-import {FetchEnvironmentService} from '@app/core/services/fetch/environment';
-import {AccordionModule} from 'primeng/accordion';
-import {RouterLink} from '@angular/router';
-import {TagModule} from 'primeng/tag';
-import {IconsModule} from 'icons.module';
-import {ButtonModule} from 'primeng/button';
-import {InputTextModule} from 'primeng/inputtext';
-import {EnvironmentListComponent} from '@app/pages/environment-list/environment-list.component';
+import { CommonModule } from '@angular/common';
+import { PipelineComponent } from '@app/components/pipeline/pipeline.component';
+import { FetchEnvironmentService } from '@app/core/services/fetch/environment';
+import { AccordionModule } from 'primeng/accordion';
+import { RouterLink } from '@angular/router';
+import { TagModule } from 'primeng/tag';
+import { IconsModule } from 'icons.module';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { EnvironmentListComponent } from '@app/pages/environment-list/environment-list.component';
 
 @Component({
   selector: 'app-pull-request-pipeline',
   imports: [InputTextModule, AccordionModule, CommonModule, RouterLink, TagModule, IconsModule, ButtonModule, PipelineComponent, MarkdownPipe, EnvironmentListComponent],
   providers: [FetchEnvironmentService],
   templateUrl: './pull-request-pipeline.component.html',
-  styleUrl: './pull-request-pipeline.component.css'
 })
 export class PullRequestPipelineComponent {
   pullRequestId: Signal<number> = input.required();
