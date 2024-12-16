8296757e589a78310d356207691d24f19cbee6d1
Turker Koc <turker.koc@tum.de>
2024-12-16 22:51:36 +0100

    environment edit final

--- client/src/app/pages/environment-edit/environment-edit.component.ts
+++ client/src/app/pages/environment-edit/environment-edit.component.ts
@@ -2,71 +2,29 @@ import { CommonModule } from '@angular/common';
 import { Component, inject, input, Signal } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { FetchEnvironmentService } from '@app/core/services/fetch/environment';
-import { EnvironmentEditFormComponent } from '../../components/forms/environment-edit-form/environment-edit-form.component';
+// import { EnvironmentEditFormComponent } from '../../components/forms/environment-edit-form/environment-edit-form.component';
 import { EnvironmentControllerService } from '@app/core/modules/openapi/api/environment-controller.service';
 import { injectQuery } from '@tanstack/angular-query-experimental';
 import { catchError, tap } from 'rxjs';
-import { ConnectedOverlayScrollHandler } from 'primeng/dom';
+import { EnvironmentEditFormComponent } from '@app/components/forms/environment-edit-form/environment-edit-form.component';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 
 @Component({
   selector: 'app-environment-edit',
-  imports: [CommonModule],
+  imports: [CommonModule, EnvironmentEditFormComponent],
   providers: [FetchEnvironmentService],
   templateUrl: './environment-edit.component.html',
   styleUrl: './environment-edit.component.css',
 })
 export class EnvironmentEditComponent {
   route = inject(ActivatedRoute);
-  environmentService = inject(EnvironmentControllerService);
-  // environmentStore = inject(EnvironmentStoreService);
-  id: Signal<string> = input.required();    
-  environemnts = {}
+  id!: string  
 
   constructor() {
-    console.log('EnvironmentEditComponent initialized');
-    console.log('Environment ID:', this.id);
-    this.updateEnvironmentQuery.refetch();
+    const routeId = this.route.snapshot.paramMap.get('id');
+    if (routeId) {
+      this.id = routeId;
+    }
   }
-
-  query = injectQuery(() => ({
-    queryKey: ['environment'],
-    queryFn: () => {
-      console.log('Executing query for environment ID:', this.id());
-      return this.environmentService.getEnvironmentById(Number(this.id()))
-        .pipe(
-          tap ((data: any) => {
-            console.log('Fetched Environment:', data);
-            this.environemnts = data;
-          }),
-          catchError((error) => {
-            console.error('Error fetching environment:', error);
-            return [];
-          })
-        ).subscribe();
-    }
-  }));
-
-  updateEnvironmentQuery = injectQuery(() => ({
-    queryKey: ['updateEnvironment'],
-    queryFn: () => {
-      const updateBody = {
-        id: Number(this.id()),
-        name: 'production', // Replace with the actual environment name
-        installedApps: ["App1", "App2", "App3"],
-        serverUrl: 'https://example.com',
-      };
-      console.log('Updating environment with:', updateBody);
-      return this.environmentService.updateEnvironment(Number(this.id()), updateBody)
-        .pipe(
-          tap((response: any) => {
-            console.log('Environment updated:', response);
-          }),
-          catchError((error) => {
-            console.error('Error updating environment:', error);
-            return [];
-          })
-        ).subscribe();
-    }
-  }));
 }
