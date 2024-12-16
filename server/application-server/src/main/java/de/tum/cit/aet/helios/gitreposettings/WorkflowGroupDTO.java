f52d357e73c2f6d7b4918f2a85a58fc3fd3966b1
Ege Kocabas <kocabas.ege@tum.de>
2024-12-16 19:43:14 +0100

    server changes for project settings

---
+++ server/application-server/src/main/java/de/tum/cit/aet/helios/gitreposettings/WorkflowGroupDTO.java
@@ -0,0 +1,26 @@
+package de.tum.cit.aet.helios.gitreposettings;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.validation.constraints.NotBlank;
+import org.springframework.lang.NonNull;
+
+import java.util.List;
+
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public record WorkflowGroupDTO(
+        @NonNull Long id,
+        @NotBlank String name,
+        @NonNull Integer orderIndex,
+        List<WorkflowMembershipDTO> memberships) {
+
+    public static WorkflowGroupDTO fromWorkflowGroup(WorkflowGroup workflowGroup) {
+        return new WorkflowGroupDTO(
+                workflowGroup.getId(),
+                workflowGroup.getName(),
+                workflowGroup.getOrderIndex(),
+                workflowGroup.getMemberships().stream()
+                        .map(WorkflowMembershipDTO::fromMembership)
+                        .toList()
+        );
+    }
+}
---
+++ server/application-server/src/main/java/de/tum/cit/aet/helios/gitreposettings/WorkflowGroupRepository.java
@@ -0,0 +1,44 @@
+package de.tum.cit.aet.helios.gitreposettings;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
+
+@Repository
+public interface WorkflowGroupRepository extends JpaRepository<WorkflowGroup, Long> {
+
+    // Find all WorkflowGroups by Repository ID
+    @Query("SELECT wg " +
+            "FROM WorkflowGroup wg " +
+            "WHERE wg.gitRepoSettings.repository.id = :repositoryId " +
+            "ORDER BY wg.orderIndex ASC")
+    List<WorkflowGroup> findAllByRepositoryId(@Param("repositoryId") Long repositoryId);
+
+    // Find all WorkflowGroups by GitRepoSettings
+    @Query("SELECT wg FROM WorkflowGroup wg WHERE wg.gitRepoSettings = :gitRepoSettings ORDER BY wg.orderIndex ASC")
+    List<WorkflowGroup> findAllByGitRepoSettings(@Param("gitRepoSettings") GitRepoSettings gitRepoSettings);
+
+    // Find the latest order index for a given GitRepoSettings
+    // If no order index is found, return -1
+    @Query("SELECT COALESCE(MAX(wg.orderIndex), -1) " +
+            "FROM WorkflowGroup wg " +
+            "WHERE wg.gitRepoSettings = :gitRepoSettings")
+    Optional<Integer> findLatestOrderIndexByGitRepoSettings(@Param("gitRepoSettings") GitRepoSettings gitRepoSettings);
+
+    // Decrement order indexes by 1 after a deleted order index
+    @Modifying
+    @Query("UPDATE WorkflowGroup wg " +
+            "SET wg.orderIndex = wg.orderIndex - 1 " +
+            "WHERE wg.gitRepoSettings = :gitRepoSettings " +
+            "AND wg.orderIndex > :deletedOrderIndex")
+    void decrementOrderIndexesAfter(@Param("deletedOrderIndex") int deletedOrderIndex,
+                                    @Param("gitRepoSettings") GitRepoSettings gitRepoSettings);
+
+}
