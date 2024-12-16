a441a99a97ed03aec9219f1e133a8cd16410da4b
Turker Koc <turker.koc@tum.de>
2024-12-16 06:18:22 +0300

    edit environments to add installed_apps

--- server/application-server/src/main/java/de/tum/cit/aet/helios/environment/Environment.java
+++ server/application-server/src/main/java/de/tum/cit/aet/helios/environment/Environment.java
@@ -9,6 +9,7 @@ import lombok.ToString;
 import de.tum.cit.aet.helios.gitrepo.GitRepository;
 
 import java.time.OffsetDateTime;
+import java.util.List;
 
 @Entity
 @Table(name = "environment")
@@ -38,6 +39,11 @@ public class Environment {
     @ManyToOne
     @JoinColumn(name = "repository_id", nullable = false)
     private GitRepository repository;
+
+    @ElementCollection
+    @CollectionTable(name = "installed_apps", joinColumns = @JoinColumn(name = "environment_id"))
+    @Column(name = "app_name")
+    private List<String> installedApps;
 
     // Missing properties
     // nodeId --> GraphQl ID
--- server/application-server/src/main/java/de/tum/cit/aet/helios/environment/EnvironmentService.java
+++ server/application-server/src/main/java/de/tum/cit/aet/helios/environment/EnvironmentService.java
@@ -34,4 +34,37 @@ public class EnvironmentService {
                 .collect(Collectors.toList());
     }
 
+    public Optional<EnvironmentDTO> updateEnvironment(Long id, EnvironmentDTO environmentDTO) {
+        return environmentRepository.findById(id).map(environment -> {
+            if (environmentDTO.name() != null) {
+                environment.setName(environmentDTO.name());
+            }
+            if (environmentDTO.url() != null) {
+                environment.setUrl(environmentDTO.url());
+            }
+            if (environmentDTO.htmlUrl() != null) {
+                environment.setHtmlUrl(environmentDTO.htmlUrl());
+            }
+            if (environmentDTO.createdAt() != null) {
+                environment.setCreatedAt(environmentDTO.createdAt());
+            }
+            if (environmentDTO.updatedAt() != null) {
+                environment.setUpdatedAt(environmentDTO.updatedAt());
+            }
+            if (environmentDTO.installedApps() != null) {
+                environment.setInstalledApps(environmentDTO.installedApps());
+            }
+            environmentRepository.save(environment);
+            return EnvironmentDTO.fromEnvironment(environment);
+        });
+    }
+
+    public Optional<EnvironmentDTO> updateInstalledApps(Long id, List<String> installedApps) {
+        return environmentRepository.findById(id).map(environment -> {
+            environment.setInstalledApps(installedApps);
+            environmentRepository.save(environment);
+            return EnvironmentDTO.fromEnvironment(environment);
+        });
+    }
+
 }
