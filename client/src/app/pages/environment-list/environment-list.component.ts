fae8c1b71ad3e82d35f02ee97f9381a6d2863fa7
bakirova <galiiabanu.bakirova@tum.de>
2024-12-17 13:26:26 +0100

    fix: pass projectid in the URL

--- client/src/app/pages/environment-list/environment-list.component.ts
+++ client/src/app/pages/environment-list/environment-list.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 import { EnvironmentListViewComponent } from '@app/components/environments/environment-list/environment-list-view.component';
 
 @Component({
