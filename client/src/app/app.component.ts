1444e4d273cd61587fa14ca641d90611af0aafea
Paul Thiel <pa.thiel@tum.de>
2024-12-18 02:38:26 +0100

    refactor: main layout

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { KeycloakService } from './core/services/keycloak/keycloak.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  template: '<router-outlet />'
+  templateUrl: './app.component.html',
 })
 export class AppComponent {}
