4ba82227f247e8bdd90c9279240ae9204ce02ba2
bakirova <galiiabanu.bakirova@tum.de>
2024-12-22 02:48:56 +0100

    fix: don't overwrite github rest api authentication header

--- client/src/app/core/services/keycloak/bearer-interceptor.ts
+++ client/src/app/core/services/keycloak/bearer-interceptor.ts
@@ -1,17 +1,19 @@
-import {Injectable} from '@angular/core';
-import {HttpEvent, HttpHandler, HttpHeaders, HttpInterceptor, HttpRequest} from '@angular/common/http';
-import {Observable} from 'rxjs';
-import {KeycloakService} from '../keycloak/keycloak.service';
+import { Injectable } from '@angular/core';
+import { HttpEvent, HttpHandler, HttpHeaders, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { KeycloakService } from '../keycloak/keycloak.service';
 
 @Injectable()
 export class BearerInterceptor implements HttpInterceptor {
     constructor(
         private keycloakService: KeycloakService
-    ) {}
+    ) { }
 
     intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
         const token = this.keycloakService.keycloak.token;
-        if (token) {
+        // TODO: Galiia -> find a better solutions 
+        // this was overwriting the github token for getting the repositories 
+        if (token && !request.headers.has('Authorization')) {
             const authReq = request.clone({
                 headers: new HttpHeaders({
                     Authorization: `Bearer ${token}`
