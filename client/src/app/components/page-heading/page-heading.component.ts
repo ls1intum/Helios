9c317a4f88b9419e3e1223b83e5053e5d6019433
Paul Thiel <10695477+thielpa@users.noreply.github.com>
2025-01-04 19:44:50 +0100

    refactor: Page layout and include repository data (#137)

---
+++ client/src/app/components/page-heading/page-heading.component.ts
@@ -0,0 +1,18 @@
+import { Component, input } from '@angular/core';
+import { getRepositoryByIdOptions } from '@app/core/modules/openapi/@tanstack/angular-query-experimental.gen';
+import { injectQuery } from '@tanstack/angular-query-experimental';
+import { DividerModule } from 'primeng/divider';
+
+@Component({
+  selector: 'app-page-heading',
+  imports: [DividerModule],
+  templateUrl: './page-heading.component.html',
+})
+export class PageHeadingComponent {
+  repositoryId = input<number>();
+
+  repositoryQuery = injectQuery(() => ({
+    ...getRepositoryByIdOptions({ path: { id: this.repositoryId() || 0 } }),
+    enabled: () => !!this.repositoryId(),
+  }));
+}
