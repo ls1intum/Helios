88953d475ba8b05fb08f695e916c40f8792a67ab
bakirova <galiiabanu.bakirova@tum.de>
2025-01-12 17:25:09 +0100

    feat: hardcode helios developers to have admin permissions

--- client/src/app/core/services/permission.service.ts
+++ client/src/app/core/services/permission.service.ts
@@ -16,8 +16,11 @@ export class PermissionService {
     enabled: () => !!this.repositoryService.currentRepositoryId() && !!this.keycloak.isLoggedIn(),
   }));
 
+  heliosDevelopers = ['gbanu', 'thielpa', 'egekocabas', 'turkerkoc', 'stefannemeth'];
+  isHeliosDeveloper = computed(() => !!this.keycloak.profile?.username && this.heliosDevelopers.includes(this.keycloak.profile.username.toLowerCase()));
+
   hasWritePermission = computed(() => this.permissionsQuery.data()?.permission === 'WRITE' || this.permissionsQuery.data()?.permission === 'ADMIN');
-  isAtLeastMaintainer = computed(() => this.permissionsQuery.data()?.permission === 'ADMIN' || this.permissionsQuery.data()?.roleName === 'maintain');
+  isAtLeastMaintainer = computed(() => this.permissionsQuery.data()?.permission === 'ADMIN' || this.permissionsQuery.data()?.roleName === 'maintain' || this.isHeliosDeveloper());
 
   refetchPermissions() {
     this.permissionsQuery.refetch();
