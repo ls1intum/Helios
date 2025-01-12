25a384bdbd3dca410af38407bc09e61948bacf27
Ege Kocabas <kocabas.ege@tum.de>
2025-01-12 14:41:19 +0100

    add description

--- client/src/app/pages/environment-edit/environment-edit.component.ts
+++ client/src/app/pages/environment-edit/environment-edit.component.ts
@@ -1,11 +1,13 @@
 import { Component, input, numberAttribute } from '@angular/core';
 import { EnvironmentEditFormComponent } from '@app/components/forms/environment-edit-form/environment-edit-form.component';
+import {PageHeadingComponent} from '@app/components/page-heading/page-heading.component';
 
 @Component({
   selector: 'app-environment-edit',
-  imports: [EnvironmentEditFormComponent],
+  imports: [EnvironmentEditFormComponent, PageHeadingComponent],
   templateUrl: './environment-edit.component.html',
 })
 export class EnvironmentEditComponent {
+  repositoryId = input.required({ transform: numberAttribute });
   id = input.required({ transform: numberAttribute });
 }
