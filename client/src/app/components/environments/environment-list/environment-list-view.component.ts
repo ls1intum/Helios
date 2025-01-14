5f026e13c9f0a57f2a408caa85390f310cadd84a
Turker Koc <39654393+TurkerKoc@users.noreply.github.com>
2025-01-14 01:13:43 +0100

    feat: Multiple deployment with one locking (#201)

--- client/src/app/components/environments/environment-list/environment-list-view.component.ts
+++ client/src/app/components/environments/environment-list/environment-list-view.component.ts
@@ -1,19 +1,22 @@
 import { Component, computed, inject, input, output, signal } from '@angular/core';
 import { AccordionModule } from 'primeng/accordion';
 
-import { RouterLink } from '@angular/router';
-import { EnvironmentDto } from '@app/core/modules/openapi';
-import { getAllEnvironmentsOptions, getAllEnvironmentsQueryKey, unlockEnvironmentMutation } from '@app/core/modules/openapi/@tanstack/angular-query-experimental.gen';
-import { injectMutation, injectQuery, injectQueryClient } from '@tanstack/angular-query-experimental';
+import { TagModule } from 'primeng/tag';
 import { IconsModule } from 'icons.module';
 import { ButtonModule } from 'primeng/button';
-import { InputTextModule } from 'primeng/inputtext';
-import { TagModule } from 'primeng/tag';
-import { EnvironmentDeploymentInfoComponent } from '../deployment-info/environment-deployment-info.component';
-import { DeploymentStateTagComponent } from '../deployment-state-tag/deployment-state-tag.component';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ConfirmationService } from 'primeng/api';
+import { RouterLink } from '@angular/router';
+import { injectMutation, injectQuery, injectQueryClient } from '@tanstack/angular-query-experimental';
 import { LockTagComponent } from '../lock-tag/lock-tag.component';
+import { DeploymentStateTagComponent } from '../deployment-state-tag/deployment-state-tag.component';
+import { InputTextModule } from 'primeng/inputtext';
+import { EnvironmentDeploymentInfoComponent } from '../deployment-info/environment-deployment-info.component';
+import { getAllEnvironmentsOptions, getAllEnvironmentsQueryKey, unlockEnvironmentMutation } from '@app/core/modules/openapi/@tanstack/angular-query-experimental.gen';
+import { EnvironmentDto } from '@app/core/modules/openapi';
 import { LockTimeComponent } from '../lock-time/lock-time.component';
 import { KeycloakService } from '@app/core/services/keycloak/keycloak.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-environment-list-view',
@@ -28,12 +31,15 @@ import { KeycloakService } from '@app/core/services/keycloak/keycloak.service';
     DeploymentStateTagComponent,
     EnvironmentDeploymentInfoComponent,
     LockTimeComponent,
+    ConfirmDialogModule,
+    CommonModule,
   ],
   templateUrl: './environment-list-view.component.html',
 })
 export class EnvironmentListViewComponent {
   private queryClient = injectQueryClient();
   private keycloakService = inject(KeycloakService);
+  private confirmationService = inject(ConfirmationService);
 
   editable = input<boolean | undefined>();
   deployable = input<boolean | undefined>();
@@ -56,13 +62,23 @@ export class EnvironmentListViewComponent {
 
   isLoggedIn = computed(() => this.keycloakService.isLoggedIn());
 
+  isCurrentUserLocked = (environment: EnvironmentDto) => {
+    return environment.lockedBy === this.keycloakService.getUserId();
+  };
+
   onUnlockEnvironment(event: Event, environment: EnvironmentDto) {
     this.unlockEnvironment.mutate({ path: { id: environment.id } });
     event.stopPropagation();
   }
 
   deployEnvironment(environment: EnvironmentDto) {
-    this.deploy.emit(environment);
+    this.confirmationService.confirm({
+      header: 'Deployment',
+      message: `Are you sure you want to deploy to ${environment.name}?`,
+      accept: () => {
+        this.deploy.emit(environment);
+      },
+    });
   }
 
   onSearch(event: Event) {
