dd7c0d3aac77989f92a0195e33ac932c8c247945
Ege Kocabas <kocabas.ege@tum.de>
2025-01-14 20:04:38 +0100

    chore: Update domain to helios.aet.cit.tum.de

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
-  serverUrl: 'https://helios.artemis.cit.tum.de',
+  serverUrl: 'https://helios.aet.cit.tum.de',
   keycloak: {
-    url: 'https://helios.artemis.cit.tum.de',
+    url: 'https://helios.aet.cit.tum.de',
     realm: 'helios',
     clientId: 'helios-app',
     skipLoginPage: true,
   },
-  clientUrl: 'https://helios.artemis.cit.tum.de',
+  clientUrl: 'https://helios.aet.cit.tum.de',
 };
