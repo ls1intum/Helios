4c6ca3ad6f3dd718fd01cca037161a42530c21b5
bakirova <galiiabanu.bakirova@tum.de>
2025-01-18 21:43:45 +0100

    feat: add custom method annotations

---
+++ server/application-server/src/main/java/de/tum/cit/aet/helios/config/security/annotations/EnforceWritePermission.java
@@ -0,0 +1,19 @@
+package de.tum.cit.aet.helios.config.security.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.security.access.prepost.PreAuthorize;
+
+/**
+ * This annotation is used to enforce that the user has write permission. It should only be used
+ * with endpoints starting with {@code /api/}
+ *
+ * <p>It's only addable to methods. The intention is that a developer can see the required role
+ * without the need to scroll up. This also prevents overrides of the annotation.
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@PreAuthorize("hasRole('WRITE') or hasRole('MAINTAINER') or hasRole('ADMIN')")
+public @interface EnforceWritePermission {}
