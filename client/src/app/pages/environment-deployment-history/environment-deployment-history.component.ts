18af68e470de35ff270cb1ac81ce07c15ef24bd4
Turker Koc <39654393+TurkerKoc@users.noreply.github.com>
2025-01-26 19:27:06 +0100

    activity history with user github profile link

--- client/src/app/pages/environment-deployment-history/environment-deployment-history.component.ts
+++ client/src/app/pages/environment-deployment-history/environment-deployment-history.component.ts
@@ -9,18 +9,32 @@ import { DateService } from '@app/core/services/date.service';
 import { CommonModule } from '@angular/common';
 import { PageHeadingComponent } from '@app/components/page-heading/page-heading.component';
 import { AvatarModule } from 'primeng/avatar';
+import { KeycloakService } from '@app/core/services/keycloak/keycloak.service';
+import { TooltipModule } from 'primeng/tooltip';
 
 @Component({
   selector: 'app-environment-deployment-history',
-  imports: [CommonModule, IconsModule, PrimeTemplate, SkeletonModule, TableModule, PageHeadingComponent, AvatarModule],
+  imports: [CommonModule, IconsModule, PrimeTemplate, SkeletonModule, TableModule, PageHeadingComponent, AvatarModule, TooltipModule],
   templateUrl: './environment-deployment-history.component.html',
 })
 export class EnvironmentDeploymentHistoryComponent {
   dateService = inject(DateService);
+  keycloakService = inject(KeycloakService);
 
   environmentId = input.required({ transform: numberAttribute });
   repositoryId = input.required({ transform: numberAttribute });
 
   activityHistoryQuery = injectQuery(() => getActivityHistoryByEnvironmentIdOptions({ path: { environmentId: this.environmentId() } }));
   activityHistory = computed(() => this.activityHistoryQuery.data());
+
+  getAvatarBorderClass(login: string) {
+    return this.keycloakService.isCurrentUser(login) ? 'border-2 border-primary-400 rounded-full' : '';
+  }
+
+  openUserProfile(login: string) {
+    //Redirect to the user's github profile
+    window.open(`
+      https://www.github.com/${login}
+    `);
+  }
 }
