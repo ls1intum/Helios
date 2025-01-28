d8065d21e532cb0423b794d0d05d79160b4951f5
Turker Koc <39654393+TurkerKoc@users.noreply.github.com>
2025-01-28 16:22:19 +0100

    deployment info tag updated

--- client/src/app/components/environments/deployment-state-tag/deployment-state-tag.component.ts
+++ client/src/app/components/environments/deployment-state-tag/deployment-state-tag.component.ts
@@ -1,10 +1,11 @@
 import { Component, input } from '@angular/core';
 import { IconsModule } from 'icons.module';
 import { TagModule } from 'primeng/tag';
+import { TooltipModule } from 'primeng/tooltip';
 
 @Component({
   selector: 'app-deployment-state-tag',
-  imports: [TagModule, IconsModule],
+  imports: [TagModule, IconsModule, TooltipModule],
   providers: [],
   templateUrl: './deployment-state-tag.component.html',
 })
