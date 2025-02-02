3ed6df0bb81f5e5967ca7b8e563027d65fe71603
Ege Kocabas <kocabas.ege@tum.de>
2025-02-02 05:22:17 +0100

    new util methods added to dateutil

--- server/application-server/src/main/java/de/tum/cit/aet/helios/util/DateUtil.java
+++ server/application-server/src/main/java/de/tum/cit/aet/helios/util/DateUtil.java
@@ -1,11 +1,31 @@
 package de.tum.cit.aet.helios.util;
 
+import java.time.Instant;
 import java.time.OffsetDateTime;
+import java.time.ZoneId;
 import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 public class DateUtil {
+
   public static OffsetDateTime convertToOffsetDateTime(Date date) {
     return date != null ? date.toInstant().atOffset(ZoneOffset.UTC) : null;
+  }
+
+  // New: Convert an Instant to OffsetDateTime using UTC by default
+  public static OffsetDateTime convertToOffsetDateTime(Instant instant) {
+    return instant != null ? instant.atOffset(ZoneOffset.UTC) : null;
+  }
+
+  // New: Convert an Instant to OffsetDateTime using a specified ZoneId
+  public static OffsetDateTime convertToOffsetDateTime(Instant instant, ZoneId zone) {
+    return instant != null ? instant.atZone(zone).toOffsetDateTime() : null;
+  }
+
+  // Format an OffsetDateTime using the given pattern
+  public static String format(OffsetDateTime offsetDateTime, String pattern) {
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
+    return formatter.format(offsetDateTime);
   }
 }
