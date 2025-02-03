c9f2485297c9901fd5f73c175916e9fedc413c22
Stefan Nemeth <stefan@bizfactory.tech>
2025-02-03 22:43:57 +0100

    Fix codestyle

--- server/application-server/src/main/java/de/tum/cit/aet/helios/tag/TagEvaluationRepository.java
+++ server/application-server/src/main/java/de/tum/cit/aet/helios/tag/TagEvaluationRepository.java
@@ -8,5 +8,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface TagEvaluationRepository extends JpaRepository<TagEvaluation, TagEvaluationId> {
   List<TagEvaluation> findByTag(Tag tag);
+
   Optional<TagEvaluation> findByTagAndEvaluatedById(Tag tag, Long userId);
 }
