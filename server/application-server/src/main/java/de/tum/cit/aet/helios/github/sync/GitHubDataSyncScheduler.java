46b2d423a7d744925d34a4c70d76752060dece36
Ege Kocabas <kocabas.ege@tum.de>
2025-02-08 01:51:12 +0100

    update data sync scheduler

--- server/application-server/src/main/java/de/tum/cit/aet/helios/github/sync/GitHubDataSyncScheduler.java
+++ server/application-server/src/main/java/de/tum/cit/aet/helios/github/sync/GitHubDataSyncScheduler.java
@@ -5,6 +5,7 @@ import de.tum.cit.aet.helios.gitrepo.GitRepoRepository;
 import de.tum.cit.aet.helios.gitrepo.GitRepository;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -48,15 +49,19 @@ public class GitHubDataSyncScheduler {
 
   public void syncData() {
     Stream<String> envRepoNames = Arrays.stream(repositoriesToMonitor);
+    log.info("Repositories from environment: {}", Arrays.toString(repositoriesToMonitor));
 
-    Stream<String> dbRepoNames = gitRepositoryRepository.findAll().stream()
-        .map(GitRepository::getNameWithOwner);
+    List<String> dbRepoList = gitRepositoryRepository.findAll().stream()
+        .map(GitRepository::getNameWithOwner)
+        .collect(Collectors.toList());
+    log.info("Repositories from database: {}", dbRepoList);
 
-    List<String> uniqueRepoNames = Stream.concat(envRepoNames, dbRepoNames)
-        .distinct()
-        .toList();
+    List<String> uniqueRepoNames =
+        Stream.concat(Arrays.stream(repositoriesToMonitor), dbRepoList.stream())
+            .distinct()
+            .toList();
 
-    log.info("Found {} unique repositories to sync.", uniqueRepoNames.size());
+    log.info("Found {} unique repositories to sync: {}", uniqueRepoNames.size(), uniqueRepoNames);
 
     uniqueRepoNames.forEach(repoName -> {
       try {
