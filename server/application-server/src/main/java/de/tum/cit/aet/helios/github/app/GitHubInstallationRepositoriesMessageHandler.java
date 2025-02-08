be4fffdc9a02e1b333e9e253b1e10e93df48078b
Ege Kocabas <kocabas.ege@tum.de>
2025-02-08 01:51:00 +0100

    webhook listener

--- server/application-server/src/main/java/de/tum/cit/aet/helios/github/app/GitHubInstallationRepositoriesMessageHandler.java
+++ server/application-server/src/main/java/de/tum/cit/aet/helios/github/app/GitHubInstallationRepositoriesMessageHandler.java
@@ -3,8 +3,9 @@ package de.tum.cit.aet.helios.github.app;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import de.tum.cit.aet.helios.github.GitHubClientManager;
 import de.tum.cit.aet.helios.github.GitHubCustomMessageHandler;
-import de.tum.cit.aet.helios.github.GitHubService;
-import de.tum.cit.aet.helios.gitrepo.github.GitHubRepositorySyncService;
+import de.tum.cit.aet.helios.github.sync.GitHubDataSyncService;
+import de.tum.cit.aet.helios.gitrepo.RepositoryService;
+import java.util.List;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Component;
 
@@ -15,19 +16,19 @@ public class GitHubInstallationRepositoriesMessageHandler
 
   private final GitHubClientManager clientManager;
 
-  private final GitHubRepositorySyncService repositorySyncService;
+  private final GitHubDataSyncService dataSyncService;
 
-  private final GitHubService gitHubService;
+  private final RepositoryService repositoryService;
 
   private GitHubInstallationRepositoriesMessageHandler(
-      GitHubClientManager clientManager,
       ObjectMapper objectMapper,
-      GitHubRepositorySyncService repositorySyncService,
-      GitHubService gitHubService) {
+      GitHubClientManager clientManager,
+      GitHubDataSyncService dataSyncService,
+      RepositoryService repositoryService) {
     super(GitHubInstallationPayload.class, objectMapper);
     this.clientManager = clientManager;
-    this.repositorySyncService = repositorySyncService;
-    this.gitHubService = gitHubService;
+    this.dataSyncService = dataSyncService;
+    this.repositoryService = repositoryService;
   }
 
 
@@ -39,6 +40,11 @@ public class GitHubInstallationRepositoriesMessageHandler
       return;
     }
 
+    List<GitHubInstallationPayload.Repository> repositoriesAdded =
+        eventPayload.getRepositoriesAdded();
+    List<GitHubInstallationPayload.Repository> repositoriesRemoved =
+        eventPayload.getRepositoriesRemoved();
+
 
     log.info(
         "Received installation_repositories event "
@@ -47,11 +53,30 @@ public class GitHubInstallationRepositoriesMessageHandler
             + "repositories removed: {}, "
             + "action: {}",
         eventPayload.getInstallation().getAppId(),
-        eventPayload.getRepositoriesAdded(),
-        eventPayload.getRepositoriesRemoved(),
+        repositoriesAdded,
+        repositoriesRemoved,
         eventPayload.getAction()
     );
 
+    // Sync repositories added
+    repositoriesAdded.forEach(
+        repository -> {
+          log.info("[Installation Event Handler] Sync will start for repository: {}",
+              repository.getFullName());
+          dataSyncService.syncRepositoryData(repository.getFullName());
+          log.info("[Installation Event Handler] Sync completed for repository: {}",
+              repository.getFullName());
+        });
+
+    // Sync repositories removed
+    repositoriesRemoved.forEach(
+        repository -> {
+          log.info("[Installation Event Handler] Deletion will start for repository: {}",
+              repository.getFullName());
+          repositoryService.deleteRepository(repository.getFullName());
+          log.info("[Installation Event Handler] Deletion completed for repository: {}",
+              repository.getFullName());
+        });
   }
 
   @Override
