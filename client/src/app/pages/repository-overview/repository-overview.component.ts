92ccd53dae388142feaaba7d533752778061223c
Ege Kocabas <kocabas.ege@tum.de>
2025-02-10 03:56:42 +0100

    refactor repository overview content

--- client/src/app/pages/repository-overview/repository-overview.component.ts
+++ client/src/app/pages/repository-overview/repository-overview.component.ts
@@ -1,9 +1,7 @@
 import { Component, computed, inject, viewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { ConnectRepoComponent } from '@app/components/connect-repo/connect-repo.component';
-import { HeliosIconComponent } from '@app/components/helios-icon/helios-icon.component';
 import { PageHeadingComponent } from '@app/components/page-heading/page-heading.component';
-import { ProfileNavSectionComponent } from '@app/components/profile-nav-section/profile-nav-section.component';
 import { RepositoryInfoDto } from '@app/core/modules/openapi';
 import { getAllRepositoriesOptions } from '@app/core/modules/openapi/@tanstack/angular-query-experimental.gen';
 import { injectQuery } from '@tanstack/angular-query-experimental';
@@ -27,8 +25,6 @@ import { ToastModule } from 'primeng/toast';
     ConnectRepoComponent,
     PageHeadingComponent,
     ToastModule,
-    ProfileNavSectionComponent,
-    HeliosIconComponent,
   ],
   templateUrl: './repository-overview.component.html',
 })
@@ -36,13 +32,6 @@ export class RepositoryOverviewComponent {
   private router = inject(Router);
 
   query = injectQuery(() => getAllRepositoriesOptions());
-  repositories = computed(() => this.query.data());
-
-  readonly repositoryConnection = viewChild.required(ConnectRepoComponent);
-
-  showDialog() {
-    this.repositoryConnection().show();
-  }
 
   navigateToProject(repository: RepositoryInfoDto) {
     console.log('Navigating to project', repository);
