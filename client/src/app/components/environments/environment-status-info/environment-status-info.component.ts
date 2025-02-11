96b04d034f4eea5c80d98018caac45f3ae428eb6
Stefan Nemeth <stefan@bizfactory.tech>
2025-02-11 17:00:54 +0100

    Update time ago in status check to update regularly

--- client/src/app/components/environments/environment-status-info/environment-status-info.component.ts
+++ client/src/app/components/environments/environment-status-info/environment-status-info.component.ts
@@ -1,16 +1,33 @@
-import { Component, computed, inject, input } from '@angular/core';
+import { Component, computed, inject, input, OnDestroy, OnInit, signal } from '@angular/core';
 import { EnvironmentStatusDto } from '@app/core/modules/openapi';
 import { DateService } from '@app/core/services/date.service';
 import { TimeAgoPipe } from '@app/pipes/time-ago.pipe';
 
 @Component({
   selector: 'app-environment-status-info',
-  imports: [TimeAgoPipe],
+  providers: [TimeAgoPipe],
   templateUrl: './environment-status-info.component.html',
 })
-export class EnvironmentStatusInfoComponent {
+export class EnvironmentStatusInfoComponent implements OnInit, OnDestroy {
+  timeAgoPipe = inject(TimeAgoPipe);
+
   status = input.required<EnvironmentStatusDto>();
   dateService = inject(DateService);
+  checkedAt = computed(() => this.status().checkedAt);
+
+  // Using it as a pipe won't update the value
+  timeSinceChecked = computed(() => {
+    return this.timeAgoPipe.transform(this.checkedAt(), {
+      showSeconds: true,
+      referenceDate: this.timeNow(),
+    });
+  });
+
+  // track the current time in a signal that we update every second
+  timeNow = signal<Date>(new Date());
+
+  // store the interval ID so we can clear it later
+  private intervalId?: ReturnType<typeof setInterval>;
 
   artemisBuildInfo = computed<{ label: string; value?: string }[]>(() => {
     const status = this.status();
@@ -42,4 +59,18 @@ export class EnvironmentStatusInfoComponent {
       },
     ];
   });
+
+  ngOnInit() {
+    // Update timeNow every second
+    this.intervalId = setInterval(() => {
+      this.timeNow.set(new Date());
+    }, 1000);
+  }
+
+  ngOnDestroy() {
+    // Prevent memory leaks by clearing the interval
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
 }
