4d28adbd6b923708ab47ab72108de48426236650
Ege Kocabas <kocabas.ege@tum.de>
2025-02-11 15:08:06 +0100

    move global async / scheduling settings into its own file

---
+++ server/application-server/src/main/java/de/tum/cit/aet/helios/config/AsyncConfig.java
@@ -0,0 +1,9 @@
+package de.tum.cit.aet.helios.config;
+
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableAsync
+@EnableScheduling
+public class AsyncConfig {
+}
--- server/application-server/src/main/java/de/tum/cit/aet/helios/environment/status/EnvironmentStatusConfig.java
+++ server/application-server/src/main/java/de/tum/cit/aet/helios/environment/status/EnvironmentStatusConfig.java
@@ -9,14 +9,10 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.TaskExecutor;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
-@EnableAsync
-@EnableScheduling
 @EnableConfigurationProperties(EnvironmentStatusConfig.StatusCheckTaskExecutorConfig.class)
 public class EnvironmentStatusConfig {
   @Bean
