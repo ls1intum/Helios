4820807e83f9259f76948be51bd4d086be9a65a0
Ege Kocabas <kocabas.ege@tum.de>
2025-02-17 15:22:56 +0100

    show name of user when expanded

--- client/src/app/components/profile-nav-section/profile-nav-section.component.ts
+++ client/src/app/components/profile-nav-section/profile-nav-section.component.ts
@@ -8,13 +8,13 @@ import { ChipModule } from 'primeng/chip';
 import { DataViewModule } from 'primeng/dataview';
 import { TagModule } from 'primeng/tag';
 import { ToastModule } from 'primeng/toast';
-import { TooltipModule } from 'primeng/tooltip';
+import { Tooltip } from 'primeng/tooltip';
 import { DividerModule } from 'primeng/divider';
 import { AvatarModule } from 'primeng/avatar';
 
 @Component({
   selector: 'app-profile-nav-section',
-  imports: [ToastModule, TooltipModule, DividerModule, AvatarModule, DataViewModule, ButtonModule, TagModule, CommonModule, CardModule, ChipModule, IconsModule],
+  imports: [ToastModule, Tooltip, DividerModule, AvatarModule, DataViewModule, ButtonModule, TagModule, CommonModule, CardModule, ChipModule, IconsModule],
   templateUrl: './profile-nav-section.component.html',
 })
 export class ProfileNavSectionComponent {
@@ -28,16 +28,6 @@ export class ProfileNavSectionComponent {
 
   logout() {
     this.keycloakService.logout();
-  }
-
-  initials() {
-    if (!this.isLoggedIn()) {
-      return '';
-    }
-
-    const profile = this.keycloakService.profile;
-
-    return `${profile?.firstName?.charAt(0) ?? ''}${profile?.lastName?.charAt(0) ?? ''}`;
   }
 
   fullName() {
