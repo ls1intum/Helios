56ee99e7f2c2fdf40319e9941586262fb744622a
bakirova <galiiabanu.bakirova@tum.de>
2025-02-17 15:38:14 +0100

    fix: pass environment type null

--- client/src/app/components/forms/environment-edit-form/environment-edit-form.component.ts
+++ client/src/app/components/forms/environment-edit-form/environment-edit-form.component.ts
@@ -55,7 +55,7 @@ export class EnvironmentEditFormComponent implements OnInit {
   environmentForm!: FormGroup;
 
   environmentTypes = [
-    { label: 'None', value: undefined },
+    { label: 'None', value: null },
     { label: 'Test', value: 'TEST' },
     { label: 'Staging', value: 'STAGING' },
     { label: 'Production', value: 'PRODUCTION' },
