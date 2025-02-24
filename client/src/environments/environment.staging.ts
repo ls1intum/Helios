d6d22b4ac5e7a4d7199cd614ef3dff74206cbbdc
Turker Koc <39654393+TurkerKoc@users.noreply.github.com>
2025-02-24 01:39:14 +0100

    chore: Staging setup (#435)

---
+++ client/src/environments/environment.staging.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  production: false,
+  sentry: {
+    enabled: false,
+    dsn: '',
+  },
+  serverUrl: 'https://helios-staging.aet.cit.tum.de',
+  keycloak: {
+    url: 'https://helios-staging.aet.cit.tum.de',
+    realm: 'helios',
+    clientId: 'helios-app',
+    skipLoginPage: true,
+  },
+  clientUrl: 'https://helios-staging.aet.cit.tum.de',
+};
