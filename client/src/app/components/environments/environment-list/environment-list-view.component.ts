a74494708f3bc129d7c415d33fdc2c64fe99e852
bakirova <galiiabanu.bakirova@tum.de>
2025-02-25 16:01:12 +0100

    group deployment environments by type

--- client/src/app/components/environments/environment-list/environment-list-view.component.ts
+++ client/src/app/components/environments/environment-list/environment-list-view.component.ts
@@ -82,6 +82,7 @@ export class EnvironmentListViewComponent implements OnDestroy {
   deployable = input<boolean | undefined>();
   hideLinkToList = input<boolean | undefined>();
 
+  groupByType = computed(() => this.deployable());
   isLoggedIn = computed(() => this.keycloakService.isLoggedIn());
   isAdmin = computed(() => this.permissionService.isAdmin());
   hasUnlockPermissions = computed(() => this.permissionService.isAtLeastMaintainer());
@@ -226,6 +227,21 @@ export class EnvironmentListViewComponent implements OnDestroy {
     });
   });
 
+  environmentGroups = computed(() => {
+    const environments = this.filteredEnvironments();
+    const groups = new Map<string, EnvironmentDto[]>();
+
+    environments.forEach(environment => {
+      const type = environment.type || 'Other';
+      if (!groups.has(type)) {
+        groups.set(type, []);
+      }
+      groups.get(type)?.push(environment);
+    });
+
+    return groups;
+  });
+
   getFullUrl(url: string): string {
     if (url && !url.startsWith('http') && !url.startsWith('https')) {
       return 'http://' + url;
@@ -312,5 +328,9 @@ export class EnvironmentListViewComponent implements OnDestroy {
 
   getBranchLink(env: EnvironmentDto) {
     return ['/repo', env.repository?.id, 'ci-cd', 'branch', env.latestDeployment?.ref];
+  }
+
+  capitalizeFirstLetter(str: string): string {
+    return str.charAt(0).toUpperCase() + str.toLowerCase().slice(1);
   }
 }
