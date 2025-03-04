43c5c731eb0f21b58140ed12e1ab3a27fe1b47b5
Ege Kocabas <kocabas.ege@tum.de>
2025-03-04 20:40:08 +0100

    feat: add search for repository overview

--- client/src/app/pages/repository-overview/repository-overview.component.ts
+++ client/src/app/pages/repository-overview/repository-overview.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { PageHeadingComponent } from '@app/components/page-heading/page-heading.component';
 import { RepositoryInfoDto } from '@app/core/modules/openapi';
@@ -13,16 +13,24 @@ import { TagModule } from 'primeng/tag';
 import { ToastModule } from 'primeng/toast';
 import { Skeleton } from 'primeng/skeleton';
 import { TableModule } from 'primeng/table';
+import { TableFilterComponent } from '@app/components/table-filter/table-filter.component';
+import { FILTER_OPTIONS_TOKEN, SearchTableService } from '@app/core/services/search-table.service';
+
+const FILTER_OPTIONS = [{ name: 'All repositories', filter: (repos: RepositoryInfoDto[]) => repos }];
 
 @Component({
   selector: 'app-repository-overview',
-  imports: [DataViewModule, ButtonModule, TagModule, CardModule, ChipModule, IconsModule, PageHeadingComponent, ToastModule, Skeleton, TableModule],
+  imports: [DataViewModule, ButtonModule, TagModule, CardModule, ChipModule, IconsModule, PageHeadingComponent, ToastModule, Skeleton, TableModule, TableFilterComponent],
+  providers: [SearchTableService, { provide: FILTER_OPTIONS_TOKEN, useValue: FILTER_OPTIONS }],
   templateUrl: './repository-overview.component.html',
 })
 export class RepositoryOverviewComponent {
   private router = inject(Router);
+  searchTableService = inject(SearchTableService<RepositoryInfoDto>);
 
   query = injectQuery(() => getAllRepositoriesOptions());
+
+  filteredRepositories = computed(() => this.searchTableService.activeFilter().filter(this.query.data() || []));
 
   navigateToProject(repository: RepositoryInfoDto) {
     console.log('Navigating to project', repository);
