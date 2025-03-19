cd26469125b36bed930301abe33f9f67c109c82e
Ege Kocabas <kocabas.ege@tum.de>
2025-03-19 18:32:04 +0100

    fix

--- client/src/test-setup.ts
+++ client/src/test-setup.ts
@@ -1,7 +1,13 @@
 import '@analogjs/vitest-angular/setup-snapshots';
 import '@angular/compiler';
 
-import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
+import { platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
 import { getTestBed } from '@angular/core/testing';
+import { NgModule, provideExperimentalZonelessChangeDetection } from '@angular/core';
 
-getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
+@NgModule({
+  providers: [provideExperimentalZonelessChangeDetection()],
+})
+export class ZonelessTestModule {}
+
+getTestBed().initTestEnvironment(ZonelessTestModule, platformBrowserDynamicTesting());
