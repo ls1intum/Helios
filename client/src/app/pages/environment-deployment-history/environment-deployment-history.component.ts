39f17123ff2471042948eccc93c0db1d3717ff1a
Turker Koc <39654393+TurkerKoc@users.noreply.github.com>
2025-03-21 17:47:00 +0100

    environment name added to activity history

--- client/src/app/pages/environment-deployment-history/environment-deployment-history.component.ts
+++ client/src/app/pages/environment-deployment-history/environment-deployment-history.component.ts
@@ -4,7 +4,7 @@ import { PrimeTemplate } from 'primeng/api';
 import { SkeletonModule } from 'primeng/skeleton';
 import { TableModule } from 'primeng/table';
 import { IconsModule } from 'icons.module';
-import { getActivityHistoryByEnvironmentIdOptions } from '@app/core/modules/openapi/@tanstack/angular-query-experimental.gen';
+import { getActivityHistoryByEnvironmentIdOptions, getEnvironmentByIdOptions } from '@app/core/modules/openapi/@tanstack/angular-query-experimental.gen';
 import { DateService } from '@app/core/services/date.service';
 import { CommonModule } from '@angular/common';
 import { PageHeadingComponent } from '@app/components/page-heading/page-heading.component';
@@ -21,6 +21,9 @@ export class EnvironmentDeploymentHistoryComponent {
 
   environmentId = input.required({ transform: numberAttribute });
   repositoryId = input.required({ transform: numberAttribute });
+
+  environmentQuery = injectQuery(() => getEnvironmentByIdOptions({ path: { id: this.environmentId() } }));
+  environment = computed(() => this.environmentQuery.data());
 
   activityHistoryQuery = injectQuery(() => getActivityHistoryByEnvironmentIdOptions({ path: { environmentId: this.environmentId() } }));
   activityHistory = computed(() => this.activityHistoryQuery.data());
