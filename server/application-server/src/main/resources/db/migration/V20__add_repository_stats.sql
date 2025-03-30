60f3b44d306bc1ae1de13a30fbf56c6f6264b019
Paul Thiel <10695477+thielpa@users.noreply.github.com>
2025-03-30 14:27:19 +0200

    V20__add_repository_stats.sql aktualisieren
    Co-authored-by: Ege Kocabas <48245934+egekocabas@users.noreply.github.com>

--- server/application-server/src/main/resources/db/migration/V20__add_repository_stats.sql
+++ server/application-server/src/main/resources/db/migration/V20__add_repository_stats.sql
@@ -3,5 +3,5 @@ alter table if exists repository add column branch_count integer not null DEFAUL
 alter table if exists repository add column latest_release_tag_name varchar(255);
 alter table if exists repository add column pull_request_count integer not null DEFAULT 0;
 create table repository_contributor (contributor_id bigint not null, user_id bigint not null, primary key (contributor_id, user_id));
-alter table if exists repository_contributor add constraint FKh74u9ub2ujepmbcquonu82xb2 foreign key (user_id) references public.user;
-alter table if exists repository_contributor add constraint FKlrcu5n700fkh90dj34d4rueb9 foreign key (contributor_id) references repository;
+alter table if exists repository_contributor add constraint FKh74u9ub2ujepmbcquonu82xb2 foreign key (user_id) references public.user on delete cascade;
+alter table if exists repository_contributor add constraint FKlrcu5n700fkh90dj34d4rueb9 foreign key (contributor_id) references repository on delete cascade;
