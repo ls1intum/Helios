3ea73d908028f0b8c17f3289dc8f9d593dbcb6f2
bakirova <galiiabanu.bakirova@tum.de>
2025-04-19 18:46:57 +0200

    fix: pr comments

--- server/application-server/src/main/java/de/tum/cit/aet/helios/releaseinfo/releasecandidate/ReleaseCandidateRepository.java
+++ server/application-server/src/main/java/de/tum/cit/aet/helios/releaseinfo/releasecandidate/ReleaseCandidateRepository.java
@@ -4,10 +4,7 @@ import de.tum.cit.aet.helios.gitrepo.GitRepository;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface ReleaseCandidateRepository extends JpaRepository<ReleaseCandidate, Long> {
@@ -25,16 +22,5 @@ public interface ReleaseCandidateRepository extends JpaRepository<ReleaseCandida
 
   boolean existsByRepositoryRepositoryIdAndName(Long repositoryId, String name);
 
-  @Transactional
-  @Modifying
-  @Query("DELETE FROM ReleaseCandidate rc WHERE rc.repository.repositoryId = ?1 AND rc.name = ?2")
-  void deleteByRepositoryRepositoryIdAndName(Long repositoryId, String name);
-
-  @Transactional
-  default Optional<ReleaseCandidate> deleteAndReturnByRepositoryRepositoryIdAndName(
-      Long repositoryId, String name) {
-    Optional<ReleaseCandidate> candidate = findByRepositoryRepositoryIdAndName(repositoryId, name);
-    candidate.ifPresent(this::delete);
-    return candidate;
-  }
+  Optional<ReleaseCandidate> deleteByRepositoryRepositoryIdAndName(Long repositoryId, String name);
 }
