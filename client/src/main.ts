dab6ae8e37c9fdc2c1093c22df12c20e8c7b4ea3
Paul Thiel <10695477+thielpa@users.noreply.github.com>
2025-04-22 23:12:20 +0200

    feat: add version number to client (#672)

--- client/src/main.ts
+++ client/src/main.ts
@@ -3,11 +3,13 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import * as Sentry from '@sentry/angular';
 import { environment } from 'environments/environment';
+import { version } from 'environments/version';
 
 Sentry.init({
   enabled: environment.sentry.enabled,
   // The DSN (Data Source Name) tells the SDK where to send the events to.
   dsn: environment.sentry.dsn,
+  release: 'helios@' + version.deployed_version,
   // The browser tracing integration captures performance data
   // like throughput and latency
   integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
