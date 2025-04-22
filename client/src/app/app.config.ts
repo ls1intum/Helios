847d5359da55ce3f9e7ead4a06400d5ce0a851fe
Paul Thiel <10695477+thielpa@users.noreply.github.com>
2025-04-22 23:30:16 +0200

    refactor: improve accessibility for icons and colors (#670)

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -3,8 +3,7 @@ import { provideRouter, Router, withComponentInputBinding, withRouterConfig } fr
 import { provideQueryClient, provideTanStackQuery, QueryClient, withDevtools } from '@tanstack/angular-query-experimental';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
-import { definePreset } from '@primeng/themes';
-import Aura from '@primeng/themes/aura';
+import primeNGConfig from './primeng.config';
 
 import { routes } from './app.routes';
 import { ConfirmationService, MessageService } from 'primeng/api';
@@ -29,113 +28,7 @@ const queryClient = new QueryClient({
 export const appConfig: ApplicationConfig = {
   providers: [
     DatePipe,
-    providePrimeNG({
-      theme: {
-        preset: definePreset(Aura, {
-          semantic: {
-            primary: {
-              50: '#eaf3fd',
-              100: '#c0daf8',
-              200: '#97c1f4',
-              300: '#6da8ef',
-              400: '#438fea',
-              500: '#176bcf',
-              600: '#1561bc',
-              700: '#104b92',
-              800: '#0b3668',
-              900: '#072140',
-              950: '#030c17',
-            },
-            colorScheme: {
-              light: {
-                surface: {
-                  0: '#ffffff',
-                  50: '{neutral.50}',
-                  100: '{neutral.100}',
-                  200: '{neutral.200}',
-                  300: '{neutral.300}',
-                  400: '{neutral.400}',
-                  500: '{neutral.500}',
-                  600: '{neutral.600}',
-                  700: '{neutral.700}',
-                  800: '{neutral.800}',
-                  900: '{neutral.900}',
-                  950: '{neutral.950}',
-                },
-                primary: {
-                  color: '{primary.900}',
-                  contrastColor: '#ffffff',
-                  hoverColor: '{primary.800}',
-                  activeColor: '{primary.800}',
-                },
-                highlight: {
-                  background: '{primary.800}',
-                  focusBackground: '{primary.700}',
-                  color: '#ffffff',
-                  focusColor: '#ffffff',
-                },
-              },
-              dark: {
-                surface: {
-                  0: '#ffffff',
-                  50: '{neutral.50}',
-                  100: '{neutral.100}',
-                  200: '{neutral.200}',
-                  300: '{neutral.300}',
-                  400: '{neutral.400}',
-                  500: '{neutral.500}',
-                  600: '{neutral.600}',
-                  700: '{neutral.700}',
-                  800: '{neutral.800}',
-                  900: '{neutral.900}',
-                  950: '{neutral.950}',
-                },
-                primary: {
-                  color: '{primary.800}',
-                  contrastColor: '{primary.100}',
-                  hoverColor: '{primary.700}',
-                  activeColor: '{primary.700}',
-                },
-                highlight: {
-                  background: '{primary.800}',
-                  focusBackground: '{primary.700}',
-                  color: '#ffffff',
-                  focusColor: '#ffffff',
-                },
-              },
-            },
-          },
-
-          components: {
-            toggleswitch: {
-              colorScheme: {
-                checkedBackground: '{emerald.500}',
-                checkedHoverBackground: '{emerald.500}',
-              },
-            },
-            tabs: {
-              colorScheme: {
-                dark: {
-                  tab: {
-                    activeColor: '{primary.hoverColor}',
-                  },
-                  activeBar: {
-                    background: '{primary.hoverColor}',
-                  },
-                },
-              },
-            },
-          },
-        }),
-        options: {
-          darkModeSelector: '.dark-mode-enabled',
-          cssLayer: {
-            name: 'primeng',
-            order: 'tailwind-base, primeng, tailwind-utilities',
-          },
-        },
-      },
-    }),
+    providePrimeNG(primeNGConfig),
     provideExperimentalZonelessChangeDetection(),
     provideRouter(routes, withComponentInputBinding(), withRouterConfig({ paramsInheritanceStrategy: 'always' })),
     provideAnimationsAsync(),
