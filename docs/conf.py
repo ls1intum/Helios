7aa94edffc7bed3e050360a770dee1bd1ddb9b27
Ege Kocabas <kocabas.ege@tum.de>
2025-05-01 18:19:35 +0200

    add favicon

--- docs/conf.py
+++ docs/conf.py
@@ -85,3 +85,5 @@ html_style = 'css/style.css'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_favicon = '_static/favicon.png'
