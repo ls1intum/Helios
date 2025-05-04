2bbf095788c14dab05b092e6c5ee29531571f0a8
Ege Kocabas <kocabas.ege@tum.de>
2025-05-04 19:41:21 +0200

    notification dtos

---
+++ server/application-server/src/main/java/de/tum/cit/aet/helios/notification/email/DeploymentFailurePayload.java
@@ -0,0 +1,28 @@
+package de.tum.cit.aet.helios.notification.email;
+
+/**
+ * Payload for the {@code deployment-failure} e‑mail notification.
+ * This payload is used to inform users about a failed deployment.
+ *
+ * @param username          The username of the user who triggered the deployment.
+ * @param sourceBranch      The source branch of the deployment.
+ * @param pullRequest       The pull request associated with the deployment.
+ * @param environment       The environment where the deployment failed.
+ * @param repositoryId      The ID of the repository where the deployment occurred.
+ * @param githubWorkflowUrl The URL of the GitHub workflow run.
+ * @param repositoryName    The name of the repository where the deployment occurred.
+ */
+public record DeploymentFailurePayload(
+    String username,
+    String sourceBranch,
+    String pullRequest,
+    String environment,
+    String repositoryId,
+    String githubWorkflowUrl,
+    String repositoryName
+) implements EmailNotificationPayload {
+  @Override
+  public String template() {
+    return "deployment-failure";
+  }
+}
---
+++ server/application-server/src/main/java/de/tum/cit/aet/helios/notification/email/LockExpiredPayload.java
@@ -0,0 +1,22 @@
+package de.tum.cit.aet.helios.notification.email;
+
+/**
+ * Payload for the {@code lock-expired} e‑mail notification.
+ * This payload is used to inform users about an expired lock.
+ *
+ * @param username       The username of the user who triggered the lock.
+ * @param environment    The environment where the lock expired.
+ * @param repositoryId   The ID of the repository where the lock expired.
+ * @param repositoryName The name of the repository where the lock expired.
+ */
+public record LockExpiredPayload(
+    String username,
+    String environment,
+    String repositoryId,
+    String repositoryName
+) implements EmailNotificationPayload {
+  @Override
+  public String template() {
+    return "lock-expired";
+  }
+}
