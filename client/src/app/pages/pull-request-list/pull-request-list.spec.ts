bacfa5a212390241ad533d81fffe650003317ade
Ege Kocabas <48245934+egekocabas@users.noreply.github.com>
2025-05-28 18:30:11 +0200

    fix: conditionally show user related PR filters (#750)

--- client/src/app/pages/pull-request-list/pull-request-list.spec.ts
+++ client/src/app/pages/pull-request-list/pull-request-list.spec.ts
@@ -2,6 +2,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PullRequestListComponent } from './pull-request-list.component';
 import { importProvidersFrom } from '@angular/core';
 import { TestModule } from '@app/test.module';
+import { KeycloakService } from '@app/core/services/keycloak/keycloak.service';
+import { PAGINATED_FILTER_OPTIONS_TOKEN, PaginatedFilterOption } from '@app/core/services/paginated-table.service';
 
 describe('Integration Test Pull Request List Page', () => {
   let component: PullRequestListComponent;
@@ -10,7 +12,24 @@ describe('Integration Test Pull Request List Page', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [PullRequestListComponent],
-      providers: [importProvidersFrom(TestModule)],
+      providers: [
+        importProvidersFrom(TestModule),
+        // Mock KeycloakService
+        {
+          provide: KeycloakService,
+          useValue: {
+            isLoggedIn: () => false,
+          },
+        },
+        // Provide filtered options through a factory
+        {
+          provide: PAGINATED_FILTER_OPTIONS_TOKEN,
+          useFactory: (): PaginatedFilterOption[] => {
+            return [{ name: 'Open pull requests', value: 'OPEN' }];
+          },
+          deps: [KeycloakService],
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(PullRequestListComponent);
