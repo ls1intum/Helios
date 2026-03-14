35287985967b67e5c786f722e67d76488a100ca4
meryemefe <meryemefe.cs@gmail.com>
2026-03-14 20:18:14 +0100

    feat: add default values for pagination and filtering in WorkflowRunPageRequest

--- server/application-server/src/main/java/de/tum/cit/aet/helios/workflow/pagination/WorkflowRunPageRequest.java
+++ server/application-server/src/main/java/de/tum/cit/aet/helios/workflow/pagination/WorkflowRunPageRequest.java
@@ -8,10 +8,13 @@ import lombok.Setter;
 @Setter
 @Builder
 public class WorkflowRunPageRequest {
+  @Builder.Default
   private int page = 1;
+  @Builder.Default
   private int size = 20;
   private String sortField;
   private String sortDirection;
+  @Builder.Default
   private WorkflowRunFilterType filterType = WorkflowRunFilterType.ALL;
   private String searchTerm;
 }
